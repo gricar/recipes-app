3bd95706326dc1bb4e59a68da33dfe497f27361b
Tabata Caroline Souto <93151350+TabataSouto@users.noreply.github.com>
2022-04-01 18:34:29 -0300

    [Grupo 18] - Requisitos 60, 61, 62, 63, 64, 65 e 66 (#469)
    * Requisitos 60, 61, 62, 63, 64, 65 e 66: finalizado, implementação da página de receitas favoritas, com filtros por food, drink ou todas as receitas, botão de compartilhar que copia a URL, botão para desfavoritar (manipulando localStorage) e redirecionamento da página ao clicar na receita
    
    * Comentário sobre arquivos/códigos feito por mim (tinha esquecido)
    
    * Correção do nome da função de acordo com code review

--- src/pages/doneRecipes/DoneRecipes.js
+++ src/pages/doneRecipes/DoneRecipes.js
@@ -1,3 +1,4 @@
+// Feito por Tabata;
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/header/Header';
--- src/pages/favoriteRecipes/FavoriteRecipes.js
+++ src/pages/favoriteRecipes/FavoriteRecipes.js
@@ -1,12 +1,200 @@
-import React from 'react';
+// Feito por Tabata;
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/header/Header';
+import shareIcon from '../../images/shareIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+import { getStorage, setStorage } from '../../services/SetAndGetStorage';
 
 function FavoriteRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [favoritesRecipesFilter, setFavoritesRecipesFilter] = useState([]);
+  const [copyUrl, setCopyUrl] = useState('');
+
+  useEffect(() => {
+    setFavoriteRecipes(getStorage('favoriteRecipes'));
+  }, []);
+
+  const handleClick = (name, type, id) => {
+    if (type === 'food') {
+      navigator.clipboard.writeText(`http://localhost:3000/foods/${id}`);
+      setCopyUrl(name);
+    }
+    if (type === 'drink') {
+      navigator.clipboard.writeText(`http://localhost:3000/drinks/${id}`);
+      setCopyUrl(name);
+    }
+  };
+
+  const handleDisfavorRecipe = (id) => {
+    const disfavor = favoriteRecipes
+      .filter(({ id: idDisfavor }) => idDisfavor !== id);
+    setFavoriteRecipes(disfavor);
+    setStorage('favoriteRecipes', disfavor);
+  };
+
+  const filterFavoriteRecipes = () => {
+    if (favoritesRecipesFilter.length) {
+      return favoritesRecipesFilter;
+    }
+    return favoriteRecipes;
+  };
+
+  const handleFilter = ({ target: { name } }) => {
+    if (name === 'food') {
+      setFavoritesRecipesFilter(favoriteRecipes.filter(({ type }) => type === 'food'));
+    }
+    if (name === 'drink') {
+      setFavoritesRecipesFilter(favoriteRecipes.filter(({ type }) => type === 'drink'));
+    }
+  };
+
   return (
-    <div>
-      <Header title="Favorite Recipes" searchBtn={ false } />
-      <p>FavoriteRecipes</p>
-    </div>
+    <section>
+      <Header title="Done Recipes" searchBtn={ false } />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFavoritesRecipesFilter([]) }
+      >
+        All
+      </button>
+      <button
+        name="food"
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ handleFilter }
+      >
+        Food
+      </button>
+      <button
+        name="drink"
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ handleFilter }
+      >
+        Drinks
+      </button>
+      {
+        filterFavoriteRecipes()
+        && filterFavoriteRecipes().map(({
+          alcoholicOrNot,
+          category,
+          id,
+          image,
+          name,
+          nationality,
+          type,
+        }, index) => {
+          if (type === 'food') {
+            return (
+              <div key={ id }>
+                <div>
+                  <Link
+                    to={ `/foods/${id}` }
+                  >
+                    <img
+                      src={ image }
+                      alt={ `Receita${name}` }
+                      data-testid={ `${index}-horizontal-image` }
+                      width="100"
+                    />
+                  </Link>
+                </div>
+                <Link
+                  to={ `/foods/${id}` }
+                >
+                  <p
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    { name }
+                  </p>
+                </Link>
+                <h3
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  { `${nationality} - ${category}` }
+                </h3>
+                <button
+                  name={ name }
+                  type="button"
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  src="shareIcon"
+                  onClick={ () => handleClick(name, type, id) }
+                >
+                  {
+                    copyUrl === name
+                      ? <p>Link copied!</p>
+                      : <img src={ shareIcon } alt="compartilhar receita" />
+                  }
+                </button>
+                <button
+                  name={ name }
+                  type="button"
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  src="blackHeartIcon"
+                  onClick={ () => handleDisfavorRecipe(id) }
+                >
+                  <img src={ blackHeartIcon } alt="compartilhar receita" />
+                </button>
+              </div>
+            );
+          }
+          return (
+            <div key={ id }>
+              <div>
+                <Link
+                  to={ `/drinks/${id}` }
+                >
+                  <img
+                    src={ image }
+                    alt={ `Receita${name}` }
+                    data-testid={ `${index}-horizontal-image` }
+                    width="100"
+                  />
+                </Link>
+              </div>
+              <Link
+                to={ `/drinks/${id}` }
+              >
+                <p
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  { name }
+                </p>
+              </Link>
+              <h3
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { `${nationality}${alcoholicOrNot}` }
+              </h3>
+              <button
+                name={ name }
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src="shareIcon"
+                onClick={ () => handleClick(name, type, id) }
+              >
+                {
+                  copyUrl === name
+                    ? <p>Link copied!</p>
+                    : <img src={ shareIcon } alt="compartilhar receita" />
+                }
+              </button>
+              <button
+                name={ name }
+                type="button"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+                src="blackHeartIcon"
+                onClick={ () => handleDisfavorRecipe(id) }
+              >
+                <img src={ blackHeartIcon } alt="compartilhar receita" />
+              </button>
+            </div>
+          );
+        })
+      }
+    </section>
   );
 }
 
