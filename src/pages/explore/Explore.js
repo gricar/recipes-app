ab1e4e04e8067469549392e6b1ad22916b9e286e
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-04-01 18:36:42 -0300

    requisito 69 - finalizado

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import BottonMenu from '../../components/BottonMenu';
 import Header from '../../components/header/Header';
+import './explore.css';
 
 function Explore() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explore" searchBtn={ false } />
-      <div>
+      <div className="exploreButtons">
         <button
           type="button"
           data-testid="explore-foods"
+          onClick={ () => history.push('/explore/foods') }
         >
           Explore Foods
         </button>
         <button
           type="button"
           data-testid="explore-drinks"
+          onClick={ () => history.push('/explore/drinks') }
         >
           Explore Drinks
         </button>
