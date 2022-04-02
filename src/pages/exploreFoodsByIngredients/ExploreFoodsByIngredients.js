7f89acbbdc174b00e06bae65c4424c075a0961d5
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-04-01 21:30:58 -0300

    requisito 75 - em andamento - criado os cards

--- src/pages/exploreFoodsByIngredients/ExploreFoodsByIngredients.js
+++ src/pages/exploreFoodsByIngredients/ExploreFoodsByIngredients.js
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import BottonMenu from '../../components/BottonMenu';
 import Header from '../../components/header/Header';
-import { fetchIngredients } from '../../services/fetchFoodAndDrinkMain';
+import { fetchIngredients, fetchImage } from '../../services/fetchFoodAndDrinkMain';
 
 function ExploreFoodsByIngredients() {
   const [mealIngredients, setMealIngredients] = useState([]);
+  const [mealImage, setMealImage] = useState([]);
+
+  const getAllImages = async (ingredArr) => {
+    const promises = ingredArr.map(async ({ strIngredient }) => { // https://oieduardorabelo.medium.com/javascript-armadilhas-do-asyn-await-em-loops-1cdad44db7f0
+      const ingredientImg = await fetchImage('meal', strIngredient);
+      setMealImage((prevState) => [...prevState, ingredientImg]);
+    });
+
+    await Promise.all(promises);
+  };
 
   useEffect(() => {
-    const getMealIngredients = async () => {
+    const getIngredients = async () => {
       const TWELVE_INGREDIENTS = 12;
       const { meals } = await fetchIngredients('meal');
       const ingredArr = meals.slice(0, TWELVE_INGREDIENTS);
       setMealIngredients(ingredArr);
+      getAllImages(ingredArr);
     };
-    getMealIngredients();
+    getIngredients();
   }, []);
 
   return (
@@ -24,9 +35,15 @@ function ExploreFoodsByIngredients() {
           mealIngredients && (
             mealIngredients.map((ingredient, index) => {
               const { strIngredient } = ingredient;
+              const smallImg = mealImage[index];
               return (
                 <div key={ index } data-testid={ `${index}-ingredient-card` }>
-                  <p datat-testid={ `${index}-card-name` }>{strIngredient}</p>
+                  <img
+                    src={ smallImg }
+                    alt="ingredientImg"
+                    data-testid={ `${index}-card-img` }
+                  />
+                  <p data-testid={ `${index}-card-name` }>{strIngredient}</p>
                 </div>
               );
             })
--- src/services/fetchFoodAndDrinkMain.js
+++ src/services/fetchFoodAndDrinkMain.js
@@ -72,3 +72,10 @@ export const fetchIngredients = async (type) => {
     return data;
   } catch (error) { return error; }
 };
+
+export const fetchImage = async (type, item) => {
+  try {
+    const response = await fetch(`https://www.the${type}db.com/images/ingredients/${item}-Small.png`);
+    return response.url;
+  } catch (error) { return error; }
+};
