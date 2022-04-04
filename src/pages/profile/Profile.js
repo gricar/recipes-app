6a33c3afd795a5201838813d4db7f55373911002
Tabata <tabatac.souto@gmail.com>
2022-04-04 14:43:31 -0300

    Requisitos 82, 83, 84, 85, 86 e 87: finalizados: tela de profile, com botões para favorite e done recipes, e de logout onde ao clicar volta para o inicio e limpa o localstorage

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import BottonMenu from '../../components/BottonMenu';
 import Header from '../../components/header/Header';
+import { getStorage } from '../../services/SetAndGetStorage';
+import './Profile.css';
 
-function Profile() {
+function Profile({ history }) {
+  const [user, setEmail] = useState({});
+
+  useEffect(() => {
+    setEmail(getStorage('user'));
+  }, []);
+
   return (
-    <div>
+    <section>
       <Header title="Profile" searchBtn={ false } />
-      <p>Profile</p>
+      <div className="profile">
+        <p data-testid="profile-email">{ user.email }</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => { history.push('/'); localStorage.clear(); } }
+        >
+          Logout
+        </button>
+      </div>
       <BottonMenu />
-    </div>
+    </section>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
